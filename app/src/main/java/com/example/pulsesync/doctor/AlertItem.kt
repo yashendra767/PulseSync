1ae3edb9ce4a6efdf9fe034747c609842808b0f2
yashe <yashendraawasthi@gmail.com>
2025-05-29 13:56:08 +0530

    DataClass update

--- app/src/main/java/com/example/pulsesync/doctor/AlertItem.kt
+++ app/src/main/java/com/example/pulsesync/doctor/AlertItem.kt
@@ -3,5 +3,7 @@ package com.example.pulsesync.doctor
 data class AlertItem(
     val title: String ="",
     val description: String="",
-    val severity: String = "Low"
+    val severity: String = "Low",
+    val bedId: String = "",
+    val timestamp: Long = 0L
 )
